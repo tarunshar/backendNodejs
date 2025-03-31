7f8ecf74fc17e77a78956cd9bf2bf086cee08a33
tarunshar <tarunsharma29102000@gmail.com>
2025-03-31 12:47:05 +0530

    updated like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,24 +5,70 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
+    const { videoId } = req.params;
+    const userId = req.user.id; // Assuming authentication middleware sets req.user
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID");
+    }
+
+    const existingLike = await Like.findOne({ video: videoId, user: userId });
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(new ApiResponse(200, { liked: false }, "Video like removed"));
+    }
+
+    await Like.create({ video: videoId, user: userId });
+    res.status(201).json(new ApiResponse(201, { liked: true }, "Video liked successfully"));
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+    const { commentId } = req.params;
+    const userId = req.user.id;
 
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+    }
+
+    const existingLike = await Like.findOne({ comment: commentId, user: userId });
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(new ApiResponse(200, { liked: false }, "Comment like removed"));
+    }
+
+    await Like.create({ comment: commentId, user: userId });
+    res.status(201).json(new ApiResponse(201, { liked: true }, "Comment liked successfully"));
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+    const { tweetId } = req.params;
+    const userId = req.user.id;
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID");
+    }
+
+    const existingLike = await Like.findOne({ tweet: tweetId, user: userId });
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(new ApiResponse(200, { liked: false }, "Tweet like removed"));
+    }
+
+    await Like.create({ tweet: tweetId, user: userId });
+    res.status(201).json(new ApiResponse(201, { liked: true }, "Tweet liked successfully"));
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const userId = req.user.id;
+
+    const likedVideos = await Like.find({ user: userId, video: { $exists: true } })
+        .populate("video")
+        .lean();
+
+    res.status(200).json(new ApiResponse(200, likedVideos, "Liked videos retrieved successfully"));
 })
 
 export {
